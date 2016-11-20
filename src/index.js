f05efa3a3fa269a1157172847ffba9275e485b81
Rosh Singh <rsingh@learnvest.com>
2016-11-19 22:59:17 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyCCG4HGCoHIBEbVJ_Cojqp1fir6xHWyjY8';
+
+class App extends Component {
+	
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('punjabi')
+		
+	};
+
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render(){
+		return (
+			<div>
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
+			</div>
+		);	
+	};
+	
+};
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
